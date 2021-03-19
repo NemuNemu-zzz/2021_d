c6e7b3796d0317810628a9d7f92d40b5a83c827e
ygkn <2000ygkn0713@gmail.com>
2021-03-19 23:06:47 +0900

    仮！

---
+++ kc3_d/Assets/ygkn/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float gravity;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] GroundingCheck groundingCheck;
+
+    Rigidbody2D rb = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isGround = groundingCheck.IsGround();
+        bool clicked = Input.GetMouseButton(0) || Input.touchCount > 0;
+
+        if (clicked && rb.velocity.x == 0) {
+            rb.velocity += new Vector2(speed, 0);
+        }
+
+        if (isGround && clicked) {
+            rb.velocity += new Vector2(0, jumpSpeed);
+        }
+
+        if (!isGround) {
+            rb.velocity += new Vector2(0, -gravity);
+        }
+
+    }
+}
